247c1d7bfe21ac76641585b29dda2c86efb3106f
Deepak-900 <deepakmaharjan900@gmail.com>
2025-06-12 16:31:58 +0545

    Completed Project 1.

--- script.js
+++ script.js
@@ -27,8 +27,25 @@ fetch('components/navbar.html')
     });
   });
 
+  // Fetch footer from component file 
   fetch('components/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-container').innerHTML = data;
     })
+
+    // Show/hide button on scroll
+  window.addEventListener('scroll', function() {
+    const goToTop = document.querySelector('.go-to-up-arrow');
+    if (window.scrollY > 300) {
+      goToTop.style.display = 'block';
+    } else {
+      goToTop.style.display = 'none';
+    }
+  });
+
+  // Smooth scroll to top on click
+  document.querySelector('.go-to-up-arrow').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
